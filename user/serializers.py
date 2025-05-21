0cd46d51e029784cf3b7d35fc43e75c216f1edce
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-05-21 16:10:45 +0100

    Added pass word reset functionality

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth import get_user_model
+
+
 
 
 ALLOWED_SIGNUP_ROLES = ['user', 'doctor'] 
-
+User = get_user_model()
 class SignUpSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     role = serializers.ChoiceField(choices=ALLOWED_SIGNUP_ROLES, required=False)
@@ -53,4 +57,31 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
             'role': self.user.role,
         })
 
+        return data
+
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("No user found with this email.")
+        return value
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    uid = serializers.IntegerField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        try:
+            user = User.objects.get(pk=data['uid'])
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Invalid user ID.")
+        
+        token_generator = PasswordResetTokenGenerator()
+        if not token_generator.check_token(user, data['token']):
+            raise serializers.ValidationError("Invalid or expired token.")
+        
         return data
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from .views import SignUpView, MyTokenObtainPairView
-from .views import SignUpView, CustomTokenObtainPairView
+from .views import SignUpView, CustomTokenObtainPairView, PasswordResetConfirmView, PasswordResetRequestView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password/reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
